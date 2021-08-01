95e195fe1a26d9bff22119ffb220448c6cccf64b
Nasibullah <41543508+nasib-ullah@users.noreply.github.com>
2021-08-01 17:26:55 +0530

    updated to the latest version

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
+'''
+Module :  data
+Author:  Nasibullah (nasibullah104@gmail.com)
+Details : This module creates datasets and dataloaders suitable for feeding data to models.
+          It Currently supports MSCOCO2014. 
+          
+'''
+
 import os
 import json
 import random
@@ -11,21 +19,20 @@ from torch.nn import functional as F
 
 
 from dictionary import Vocabulary,EOS_token,PAD_token,SOS_token,UNK_token
-from utils import maskNLLLoss,normalizeString
-from config import Config,Path
 
 
 class COCO14Dataset(Dataset):
-    def __init__(self,coco,voc,transforms=None):
+    def __init__(self,utils,coco,voc,transforms=None):
         self.coco = coco
         self.voc = voc
         self.transforms = transforms
+        self.normalize = utils.normalizeString
     def __len__(self):
         return len(self.coco)
     def __getitem__(self,idx):
         img,target = self.coco[idx]
         ide = self.coco.ids[idx]
-        lbl = normalizeString(random.choice(target))
+        lbl = self.normalize(random.choice(target))
         label = []
         for s in lbl.split(' '):
             if s in list(self.voc.word2index.keys()):
@@ -52,8 +59,7 @@ class COCO2014Test(Dataset):
         if self.transform:
             img = self.transform(img)
         return ide,img
-   
-
+    
 def collate_fn(batch):
     data = [item[0] for item in batch]
     images = torch.stack(data,0)
@@ -82,10 +88,10 @@ def collate_fn(batch):
 
 class DataHandler:
     
-    def __init__(self,dataset_path, vocabulary):
-        self.dataset_path = dataset_path
-        self.voc = vocabulary
-        self.path = Path(dataset_path)
+    def __init__(self,cfg,path,voc):
+        self.cfg = cfg
+        self.voc = voc
+        self.path = path
         self.data_transform = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()])
 
         self.coco_train = dset.CocoCaptions(root=self.path.train_image_path,annFile=self.path.train_annotation_file,transform=self.data_transform)
@@ -96,18 +102,18 @@ class DataHandler:
         for img in test_info['images']:
             self.id2fname[img['id']] = img['file_name']
 
-    def getDataSets(self):     
-        train_dset = COCO14Dataset(self.coco_train,self.voc,transforms=self.data_transform)
-        val_dset = COCO14Dataset(self.coco_val,self.voc)
+    def getDataSets(self,utils):     
+        train_dset = COCO14Dataset(utils,self.coco_train,self.voc,transforms=self.data_transform)
+        val_dset = COCO14Dataset(utils,self.coco_val,self.voc)
         test_dset = COCO2014Test(self.path.test_image_path,self.id2fname,self.data_transform)
         
         return train_dset, val_dset, test_dset
     def getDataLoaders(self,train_dset,val_dset,test_dset):
 
-        train_loader=DataLoader(train_dset,batch_size = Config.batch_size, num_workers = 8,shuffle = True,
+        train_loader=DataLoader(train_dset,batch_size = self.cfg.batch_size, num_workers = 8,shuffle = True,
                             collate_fn = collate_fn, drop_last=True)
 
-        val_loader = DataLoader(val_dset,batch_size = Config.val_batch_size, num_workers = 8,shuffle = False,collate_fn = collate_fn,drop_last=False)
+        val_loader = DataLoader(val_dset,batch_size = self.cfg.val_batch_size, num_workers = 8,shuffle = False,collate_fn = collate_fn,drop_last=False)
         test_loader = DataLoader(test_dset,batch_size = 35, num_workers = 8,shuffle = False,drop_last=False)
         
         return train_loader,val_loader,test_loader
